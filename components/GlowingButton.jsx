b0cdd11614af5e2ccd2fef52fb08c5c94e740a3f
Binary Floyd <Floydify@A-Macbook-Pro.local>
2023-03-21 04:31:37 +0530

    minor button move

--- components/GlowingButton.jsx
+++ components/GlowingButton.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function GlowingButton({ label }) {
   return (
     <button
-      className="relative bg-gradient-to-r from-blue-600 to-blue-900 hover:to-blue-900 focus:outline-none focus:ring-1 focus:ring-offset-1 focus:ring-blue-700 rounded-full py-2 px-3 mt-4 text-white font-semibold shadow-lg transition ease-out hover:-translate-y-1 hover:scale-110 duration-200"
+      className="relative bg-gradient-to-r from-blue-600 to-blue-900 hover:to-blue-900 focus:outline-none focus:ring-1 focus:ring-offset-1 focus:ring-blue-700 rounded-full py-2 px-3 mt-4 text-white font-semibold shadow-lg transition ease-out hover:-translate-y-1 hover:scale-110 duration-200 space-x-2"
     >
       <span className="absolute -inset-px rounded-full bg-blue-400 opacity-75 animate-pulse"></span>
       <span className="relative">{label}</span>
