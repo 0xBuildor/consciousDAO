6b80286e417f415183db8c2b9baa12cd421f0ec7
Binary Floyd <Floydify@A-Macbook-Pro.local>
2023-03-21 00:37:43 +0530

    Test Commit

--- components/Main.jsx
+++ components/Main.jsx
@@ -6,25 +6,23 @@ import { fadeIn } from "@/utils/motion";
 export default function Main() {
     return (
         <>
-            <div className="w-full h-screen overflow-hidden">
+            <div className="h-screen overflow-hidden">
                 
-                <video autoPlay loop muted className="absolute z-10 w-full h-full object-cover " >
+                <video autoPlay loop muted className="absolute z-10 w-full h-screen object-cover " >
                     <source src="/Assets/terrain.mp4" type="video/mp4" />
                 </video>
                 
-                <motion.div variants={navVariants}
-                initial="hidden"
-                whileInView="show"
+                <div 
                 className="relative z-30 flex flex-row items-center justify-center mt-24  ">
                     <a href="#" >
                         <img
-                            className=" z-10 bg-transparent w-96 h-auto"
+                            className=" z-10 bg-transparent w-44 md:w-80 h-auto"
                             src="Assets/Logo.svg"
                             alt=""
                         />
                     </a>
                  
-                </motion.div>
+                </div>
 
                 <div
                 className="relative z-30 p-8 max-w-sm mx-auto my-20 bg-white  rounded-xl shadow-lg flex flex-col items-center space-x-4 ring-offset-2 ring-4">
@@ -32,7 +30,7 @@ export default function Main() {
                         <div className="text-xl font-medium text-black"></div>
                         <p className="text-slate-700 text-justify"> A permissionless DAO on a mission to grow and expand the CyberConnect protocol and its ecosystem. By accelerating adoption of its social graph protocol and the dApps leveraging its infrastructure, Conscious DAO will build, fund and support the next generation of social media platforms that are user and creator-centric. </p>
                     </div>
-                    <button className="rounded-full border-2 px-2 py-1 text-slate-800 text-sm mt-4 bg-white shadow-lg transition ease-out hover:-translate-y-1 hover:scale-110 duration-200 hover:text-white">💬 Join the Community</button>
+                    <button className="rounded-full border-2 px-2 py-1 text-slate-800 text-sm mt-4 bg-white shadow-lg transition ease-out hover:-translate-y-1 hover:scale-110 duration-200">💬 Join the Community</button>
 
         
                  </div>
