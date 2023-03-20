0df2410f6b948f240203c8c5f459dbc7d72bed21
Binary Floyd <Floydify@A-Macbook-Pro.local>
2023-03-21 04:17:58 +0530

    Logo

--- components/Main.jsx
+++ components/Main.jsx
@@ -17,7 +17,7 @@ export default function Main() {
                 className="relative z-30 flex flex-row items-center justify-center mt-20  ">
                     <a href="#" >
                         <img
-                            className=" z-10 bg-transparent w-80 md:w-96 h-auto"
+                            className=" z-10 bg-transparent w-72 md:w-96 h-auto"
                             src="Assets/logo.png"
                             alt=""
                         />
@@ -30,7 +30,6 @@ export default function Main() {
                <div
                 className="relative z-30 p-8 max-w-xs md:max-w-md m-10 mx-auto my-20 bg-contain bg-white  rounded-xl shadow-lg flex flex-col items-center justify-center space-x-4 ring-offset-2 ring-4">
                     <div>
-                        <div className="text-xl font-medium text-black"></div>
                         <p className="text-slate-800 text-center"> A permissionless DAO on a mission to grow and expand the CyberConnect protocol and its ecosystem. By accelerating adoption of its social graph protocol and the dApps leveraging its infrastructure, Conscious DAO will build, fund and support the next generation of social media platforms that are user and creator-centric. </p>
                     </div>
                   {/* <button className="rounded-full border-2 px-2 py-1 text-slate-800 text-sm mt-4 bg-white shadow-lg transition ease-out hover:-translate-y-1 hover:scale-110 duration-200">💬 Join the Community</button> */} 
